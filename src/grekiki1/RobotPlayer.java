293b9b8b95efec6e0e95f1f882a6ad32e7224a9b
grekiki <gregor1234567890@gmail.com>
2020-01-07 22:01:12 +0100

    Sedaj lahko moj robot precej prepričljivo premaga naključno implementacijo

--- src/grekiki1/RobotPlayer.java
+++ src/grekiki1/RobotPlayer.java
@@ -1,250 +1,192 @@
 package grekiki1;
+import java.util.Arrays;
+
 import battlecode.common.*;
 
+class vector_gl{
+	MapLocation[] q;
+	int p=0;
+	int size=20;
+	vector_gl(){
+		q=new MapLocation[20];
+	}
+	void add(MapLocation ml){
+		q[p++]=ml;
+		if(p==size){
+			q=Arrays.copyOf(q,2*size);
+			size*=2;
+		}
+	}
+	MapLocation get(int a){
+		if(a<0||a>=size){
+			System.out.println("Vector index out of bounds exception");
+			return null;
+		}else{
+			return q[a];
+		}
+	}
+}
+abstract class robot{
+	public abstract void precompute();
+	public abstract void runTurn();
+	public abstract void postcompute();
+}
+class HQ extends robot{
+	int round;
+	RobotController rc;
+	HQ(RobotController rc){
+		this.rc=rc;
+		round=rc.getRoundNum();
+	}
+	@Override public void precompute(){
+		// TODO Auto-generated method stub
+
+	}
+	@Override public void runTurn(){
+		try{
+			round++;
+			int soup=rc.getTeamSoup();
+			if(soup>=70&&rc.isReady()){
+				for(Direction d:Direction.allDirections()){
+					if(rc.canBuildRobot(RobotType.MINER,d)){
+						rc.buildRobot(RobotType.MINER,d);
+						return;
+					}
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	@Override public void postcompute(){
+		// TODO Auto-generated method stub
+
+	}
+}
+class miner extends robot{
+	vector_gl surovine=new vector_gl();
+	int round;
+	int alive=0;
+	RobotController rc;
+	MapLocation HQ;
+	miner(RobotController rc){
+		this.rc=rc;
+		round=rc.getRoundNum();
+	}
+	@Override public void precompute(){
+		alive++;
+		round++;
+		if(alive==1){
+			for(RobotInfo r:rc.senseNearbyRobots(2)){
+				if(r.team==rc.getTeam()&&r.type==RobotType.HQ){
+					HQ=r.location;
+				}
+			}
+			if(HQ==null){
+				alive--;//èe ne vemo kje je baza, a se sploh splaèa obstajati?
+			}
+		}
+	}
+	@Override public void runTurn(){
+		try{
+			if(rc.isReady()){
+				if(rc.getSoupCarrying()==100){
+					if(HQ==null){
+						System.out.println("Kje je baza?");
+					}else{
+						if(rc.canMove(rc.getLocation().directionTo(HQ))){
+							rc.move(rc.getLocation().directionTo(HQ));
+							return;
+						}
+					}
+				}
+				for(Direction d:Direction.allDirections()){
+					if(rc.canMineSoup(d)){
+						rc.mineSoup(d);
+						return;
+					}
+				}
+				for(Direction d:Direction.allDirections()){
+					if(rc.canDepositSoup(d)){
+						rc.depositSoup(d,rc.getSoupCarrying());
+						return;
+					}
+				}
+				for(int i=0;i<10;i++){
+					Direction d=Direction.allDirections()[(int)(Math.random()*Direction.allDirections().length)];
+					if(rc.canMove(d)){
+						rc.move(d);
+						return;
+					}
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	@Override public void postcompute(){
+		// TODO Auto-generated method stub
+
+	}
+
+}
 public strictfp class RobotPlayer{
 	static RobotController rc;
-
 	static Direction[] directions={Direction.NORTH,Direction.EAST,Direction.SOUTH,Direction.WEST};
-	static RobotType[] spawnedByMiner={RobotType.REFINERY,RobotType.VAPORATOR,RobotType.DESIGN_SCHOOL,RobotType.FULFILLMENT_CENTER,RobotType.NET_GUN};
 
-	static int turnCount;
+	public static void run(RobotController rrrrr) throws GameActionException{
+		rc=rrrrr;
+		System.out.println(rc.getType()+" spawned");
+		robot r=null;
+		try{
+			switch(rc.getType()){
+				case HQ:
+					r=new HQ(rc);
+					break;
+				case MINER:
+					r=new miner(rc);
+					break;
+				case REFINERY:
+//						runRefinery();
+					break;
+				case VAPORATOR:
+//						runVaporator();
+					break;
+				case DESIGN_SCHOOL:
+//						runDesignSchool();
+					break;
+				case FULFILLMENT_CENTER:
+//						runFulfillmentCenter();
+					break;
+				case LANDSCAPER:
+//						runLandscaper();
+					break;
+				case DELIVERY_DRONE:
+//						runDeliveryDrone();
+					break;
+				case NET_GUN:
+//						runNetGun();
+					break;
+				case COW:
+					break;
+				default:
+					break;
+			}
 
-	/**
-	 * run() is the method that is called when a robot is instantiated in the Battlecode world. If this method returns, the robot dies!
-	 **/
-	public static void run(RobotController rc) throws GameActionException{
-
-		// This is the RobotController object. You use it to perform actions from this robot,
-		// and to get information on its current status.
-		RobotPlayer.rc=rc;
-
-		turnCount=0;
-
-		System.out.println("I'm a "+rc.getType()+" and I just got created!");
+		}catch(Exception e){
+			System.out.println(rc.getType()+" Exception");
+			e.printStackTrace();
+		}
 		while(true){
-			turnCount+=1;
-			// Try/catch blocks stop unhandled exceptions, which cause your robot to explode
-			try{
-				// Here, we've separated the controls into a different method for each RobotType.
-				// You can add the missing ones or rewrite this into your own control structure.
-				System.out.println("I'm a "+rc.getType()+"! Location "+rc.getLocation());
-				switch(rc.getType()){
-					case HQ:
-						runHQ();
-						break;
-					case MINER:
-						runMiner();
-						break;
-					case REFINERY:
-						runRefinery();
-						break;
-					case VAPORATOR:
-						runVaporator();
-						break;
-					case DESIGN_SCHOOL:
-						runDesignSchool();
-						break;
-					case FULFILLMENT_CENTER:
-						runFulfillmentCenter();
-						break;
-					case LANDSCAPER:
-						runLandscaper();
-						break;
-					case DELIVERY_DRONE:
-						runDeliveryDrone();
-						break;
-					case NET_GUN:
-						runNetGun();
-						break;
-					case COW:
-						break;
-					default:
-						break;
-				}
-
-				// Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
-				Clock.yield();
-
-			}catch(Exception e){
-				System.out.println(rc.getType()+" Exception");
-				e.printStackTrace();
+			if(r!=null){
+				r.precompute();
+				r.runTurn();
+				r.postcompute();
+			}else{
+				System.out.println("NAPAKA!!!");
 			}
+			Clock.yield();
 		}
 	}
 
-	static void runHQ() throws GameActionException{
-		for(Direction dir:directions)
-			tryBuild(RobotType.MINER,dir);
-	}
-
-	static void runMiner() throws GameActionException{
-		tryBlockchain();
-		tryMove(randomDirection());
-		if(tryMove(randomDirection()))
-			System.out.println("I moved!");
-		// tryBuild(randomSpawnedByMiner(), randomDirection());
-		for(Direction dir:directions)
-			tryBuild(RobotType.FULFILLMENT_CENTER,dir);
-		for(Direction dir:directions)
-			if(tryRefine(dir))
-				System.out.println("I refined soup! "+rc.getTeamSoup());
-		for(Direction dir:directions)
-			if(tryMine(dir))
-				System.out.println("I mined soup! "+rc.getSoupCarrying());
-	}
-
-	static void runRefinery() throws GameActionException{
-		// System.out.println("Pollution: " + rc.sensePollution(rc.getLocation()));
-	}
-
-	static void runVaporator() throws GameActionException{
-
-	}
-
-	static void runDesignSchool() throws GameActionException{
-
-	}
-
-	static void runFulfillmentCenter() throws GameActionException{
-		for(Direction dir:directions)
-			tryBuild(RobotType.DELIVERY_DRONE,dir);
-	}
-
-	static void runLandscaper() throws GameActionException{
-
-	}
-
-	static void runDeliveryDrone() throws GameActionException{
-		Team enemy=rc.getTeam().opponent();
-		if(!rc.isCurrentlyHoldingUnit()){
-			// See if there are any enemy robots within striking range (distance 1 from lumberjack's radius)
-			RobotInfo[] robots=rc.senseNearbyRobots(GameConstants.DELIVERY_DRONE_PICKUP_RADIUS_SQUARED,enemy);
-
-			if(robots.length>0){
-				// Pick up a first robot within range
-				rc.pickUpUnit(robots[0].getID());
-				System.out.println("I picked up "+robots[0].getID()+"!");
-			}
-		}else{
-			// No close robots, so search for robots within sight radius
-			tryMove(randomDirection());
-		}
-	}
-
-	static void runNetGun() throws GameActionException{
-
-	}
-
-	/**
-	 * Returns a random Direction.
-	 *
-	 * @return a random Direction
-	 */
-	static Direction randomDirection(){
-		return directions[(int)(Math.random()*directions.length)];
-	}
-
-	/**
-	 * Returns a random RobotType spawned by miners.
-	 *
-	 * @return a random RobotType
-	 */
-	static RobotType randomSpawnedByMiner(){
-		return spawnedByMiner[(int)(Math.random()*spawnedByMiner.length)];
-	}
-
-	static boolean tryMove() throws GameActionException{
-		for(Direction dir:directions)
-			if(tryMove(dir))
-				return true;
-		return false;
-		// MapLocation loc = rc.getLocation();
-		// if (loc.x < 10 && loc.x < loc.y)
-		//     return tryMove(Direction.EAST);
-		// else if (loc.x < 10)
-		//     return tryMove(Direction.SOUTH);
-		// else if (loc.x > loc.y)
-		//     return tryMove(Direction.WEST);
-		// else
-		//     return tryMove(Direction.NORTH);
-	}
-
-	/**
-	 * Attempts to move in a given direction.
-	 *
-	 * @param dir
-	 *            The intended direction of movement
-	 * @return true if a move was performed
-	 * @throws GameActionException
-	 */
-	static boolean tryMove(Direction dir) throws GameActionException{
-		// System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
-		if(rc.isReady()&&rc.canMove(dir)){
-			rc.move(dir);
-			return true;
-		}else
-			return false;
-	}
-
-	/**
-	 * Attempts to build a given robot in a given direction.
-	 *
-	 * @param type
-	 *            The type of the robot to build
-	 * @param dir
-	 *            The intended direction of movement
-	 * @return true if a move was performed
-	 * @throws GameActionException
-	 */
-	static boolean tryBuild(RobotType type,Direction dir) throws GameActionException{
-		if(rc.isReady()&&rc.canBuildRobot(type,dir)){
-			rc.buildRobot(type,dir);
-			return true;
-		}else
-			return false;
-	}
-
-	/**
-	 * Attempts to mine soup in a given direction.
-	 *
-	 * @param dir
-	 *            The intended direction of mining
-	 * @return true if a move was performed
-	 * @throws GameActionException
-	 */
-	static boolean tryMine(Direction dir) throws GameActionException{
-		if(rc.isReady()&&rc.canMineSoup(dir)){
-			rc.mineSoup(dir);
-			return true;
-		}else
-			return false;
-	}
-
-	/**
-	 * Attempts to refine soup in a given direction.
-	 *
-	 * @param dir
-	 *            The intended direction of refining
-	 * @return true if a move was performed
-	 * @throws GameActionException
-	 */
-	static boolean tryRefine(Direction dir) throws GameActionException{
-		if(rc.isReady()&&rc.canDepositSoup(dir)){
-			rc.depositSoup(dir,rc.getSoupCarrying());
-			return true;
-		}else
-			return false;
-	}
-
-	static void tryBlockchain() throws GameActionException{
-		if(turnCount<3){
-			int[] message=new int[7];
-			for(int i=0;i<7;i++){
-				message[i]=123;
-			}
-			if(rc.canSubmitTransaction(message,10))
-				rc.submitTransaction(message,10);
-		}
-		// System.out.println(rc.getRoundMessages(turnCount-1));
-	}
 }
